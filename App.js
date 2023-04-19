b506cb9e61a0dddecbf21b1dfec3def418ef5580
berg1618 <joselindemberg5@protonmail.com>
2023-04-19 16:41:49 -0300

    adicionar imagem inicial

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <View>
+          <Image source={require("./assets/pagina_incial.png")} />
+        </View>
+
+        <Button title="Perfil" color="#747070" />
+        <Button title="Notificações" color="#747070" />
+        <Button title="Cadastrar rosto" color="#747070" />
+      </View>
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
