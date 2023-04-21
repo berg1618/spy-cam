8b41348356eff047098b40c74d4fa41160c271b0
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2023-04-21 20:41:33 -0300

    pasta components

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, Image, StyleSheet, Text, View } from "react-native";
+import Login from "./components/Login/";
 import React from "react";
 export default function App() {
   return (
@@ -8,7 +9,13 @@ export default function App() {
         <View>
           <Image source={require("./assets/pagina_incial.png")} />
           <Text>ou sou claudio</Text>
+        
         </View>
+
+        {/* ##########----############ */}
+        {/* vcs podem chamar seu componente aqui para poder testar */}
+        <Login />
+         {/* ##########----############ */}
 
         <Button title="Perfil" color="#747070" />
         <Button title="Notificações" color="#747070" />
---
+++ components/Login/index.js
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+const Login = () => {
+  return (
+    <View>
+      <View>
+        <View>
+         
+          <Text>Tela de login</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default Login
