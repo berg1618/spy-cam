6d1cf273f8a683692429b7f9f9f0169069074a04
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2023-04-21 21:42:14 -0300

    tela de login sem css

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,15 +1,53 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Image, Text, View, TextInput, TouchableOpacity } from "react-native";
+import styles from './styles'
 import React from "react";
+
 const Login = () => {
   return (
     <View>
       <View>
-        <View>
-         
-          <Text>Tela de login</Text>
-        </View>
+        <Text style={styles.titleApp}>
+          Spy Cam
+        </Text>
       </View>
+
+      <Image
+        source={require('../../assets/iconReconhecimento.png')}
+        />
+
+        <View style={styles.formContainer}>
+          <Text>Faça seu cadastro</Text>
+
+          <View style={styles.inputs}>
+            <Image
+              source={require('../../assets/iconNome.png')}
+            />
+            <TextInput
+              placeholder="nome"
+              keyboardType="text"
+            />
+          </View>
+
+         
+          <View style={styles.inputs}>
+            <Image
+              source={require('../../assets/iconPassword.png')}
+            />
+            <TextInput
+              placeholder="senha"
+              keyboardType="text"
+            />
+          </View>
+
+          <TouchableOpacity>
+              <Text>Login</Text>
+          </TouchableOpacity>
+
+          <Text>
+            nao possui cadasto? <Text style={styles.textBold}>cadasre-se</Text>
+          </Text>
+         
+        </View>
     </View>
   );
 }
