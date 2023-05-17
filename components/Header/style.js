22f6ecbcbf84bb518c4613eb2db025eb90f1a036
berg1618 <joselindemberg5@protonmail.com>
2023-05-17 13:30:41 -0300

    tela de notificacao

---
+++ components/Header/style.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+
+const styles =StyleSheet.create({
+    header : {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 150,
+        backgroundColor: 'transparent',
+        borderBottomWidth: 2,
+        borderBottomColor: '#FFFFFF',
+        position: 'relative'
+    },
+    title :{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 40,
+        textAlign: 'center',
+        position: 'absolute',
+        bottom: 30,
+        left: 0,
+        right: 0,
+        },
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
--- components/Notificacoes/index.js
+++ components/Notificacoes/index.js
@@ -1,15 +1,107 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
 import { useNavigation } from '@react-navigation/native';
 // import styles from "./styles";
 
+
+
+//array que ta simulando o back
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'ale entrou no carro',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'berg entrou no carro',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'lucas entrou no carrro',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'day entrou no carrro',
+    },
+    { 
+      
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'claudio robou o carro',
+    },
+  ];
+
+
+//vai rederizar um item
+const Item = ({item}) => (
+    <TouchableOpacity style={styles.botao}>
+      <Text>{item.title}</Text>
+    </TouchableOpacity>
+  );
+
+//parte principal
 const Notificacoes = () => {
     const navigation = useNavigation();
-     return (
-        <View style={{alignItems: "center"}}>
-            <Text>Pagina de notificacoes</Text>
-        </View>
-     )
+
+    const [selectedId, setSelectedId] = useState();
+
+    const renderItem = ({item}) => {
+        const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+        const color = item.id === selectedId ? 'white' : 'black';
+    
+        return (
+          <Item
+            item={item}
+           />
+        );
+      };
+
+      //conteudo da tela de notificacao
+      return (
+       
+        <SafeAreaView style={styles.container}>
+            <View style={styles.mainnotificacao}>
+            <Text style={styles.titleApp}>Notificações</Text>
+                <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                extraData={selectedId}
+            />
+            </View>
+         
+        </SafeAreaView>
+      );
 }
 
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+      
+    },
+    item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+    botao: {
+      backgroundColor: '#eee',
+      margin: 10,
+      padding: 10
+    },
+    mainnotificacao:{}
+  });
 export default Notificacoes
