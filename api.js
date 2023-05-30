4156be11370c950b2445c6a457514dbb34f021d7
Aluno <aluno@debian.ifrn.local>
2023-05-30 15:57:23 -0300

    consertar carregamento de variáveis de ambiente

--- api.js
+++ api.js
@@ -1,21 +1,24 @@
 import axios from "axios"
+import Config from 'react-native-config';
+
+const Url = Config.URL;
 
 const api = axios.create({
-  baseURL: 'http://10.48.7.111:8013',
+  baseURL: Url,
   timeout: 5000,
-  headers: {'X-Custom-Header': 'foobar'}
+  headers: { 'X-Custom-Header': 'foobar' }
 })
 
 
 const cadastrarUsuarioBanco = async (nome, email, senha) => {
-    const req = await api.post('usuarios', {
-        nome: nome,
-        email: email,
-        senha: senha
-      })
-      return req
+  const req = await api.post('usuarios', {
+    nome: nome,
+    email: email,
+    senha: senha
+  })
+  return req
 }
 
 export {
-    cadastrarUsuarioBanco
+  cadastrarUsuarioBanco
 }
