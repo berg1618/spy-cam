a14655ceff301471bc5801922fbc7bab2b9ddf2f
berg1618 <berg@blickdev.com.br>
2023-06-26 08:31:31 -0300

    typo

--- components/CadastrarConta/index.js
+++ components/CadastrarConta/index.js
@@ -82,7 +82,7 @@ const CadastrarConta = ({navigation}) => {
               <View style={styles.entrada}>
               <Image
                 style={styles.icon} 
-                source={require('../../assets/iconEMail.png')}
+                source={require('../../assets/iconEmail.png')}
               />
                 <TextInput
                   style={styles.inputText}
