30c469de3b4047ed40e6d0dfcff15a92358d1425
berg1618 <berg@blickdev.com.br>
2023-06-26 08:29:29 -0300

    ajustes menores

--- camera.js
+++ camera.js
@@ -1,67 +1,59 @@
-import React, { Fragment, Component } from 'react';
-import ImagePicker from 'react-native-image-picker';
+import React, { Fragment, Component } from "react";
+import ImagePicker from "react-native-image-picker";
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
   Image,
-  Button,
   Dimensions,
-  TouchableOpacity
-} from 'react-native';
+  TouchableOpacity,
+} from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const options = {
-  title: 'Select Avatar',
-  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  title: "Select Avatar",
+  customButtons: [{ name: "fb", title: "Choose Photo from Facebook" }],
   storageOptions: {
     skipBackup: true,
-    path: 'images',
+    path: "images",
   },
 };
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       filepath: {
-        data: '',
-        uri: ''
+        data: "",
+        uri: "",
       },
-      fileData: '',
-      fileUri: ''
-    }
+      fileData: "",
+      fileUri: "",
+    };
   }
 
   chooseImage = () => {
     let options = {
-      title: 'Select Image',
+      title: "Select Image",
       customButtons: [
-        { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+        { name: "customOptionKey", title: "Choose Photo from Custom Option" },
       ],
       storageOptions: {
         skipBackup: true,
-        path: 'images',
+        path: "images",
       },
     };
     ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
+      console.log("Response = ", response);
 
       if (response.didCancel) {
-        console.log('User cancelled image picker');
+        console.log("User cancelled image picker");
       } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
+        console.log("ImagePicker Error: ", response.error);
       } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
+        console.log("User tapped custom button: ", response.customButton);
         alert(response.customButton);
       } else {
         const source = { uri: response.uri };
@@ -69,99 +61,101 @@ export default class App extends Component {
         // You can also display the image using data:
         // const source = { uri: 'data:image/jpeg;base64,' + response.data };
         // alert(JSON.stringify(response));s
-        console.log('response', JSON.stringify(response));
+        console.log("response", JSON.stringify(response));
         this.setState({
           filePath: response,
           fileData: response.data,
-          fileUri: response.uri
+          fileUri: response.uri,
         });
       }
     });
-  }
+  };
 
   launchCamera = () => {
     let options = {
       storageOptions: {
         skipBackup: true,
-        path: 'images',
+        path: "images",
       },
     };
     ImagePicker.launchCamera(options, (response) => {
-      console.log('Response = ', response);
+      console.log("Response = ", response);
 
       if (response.didCancel) {
-        console.log('User cancelled image picker');
+        console.log("User cancelled image picker");
       } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
+        console.log("ImagePicker Error: ", response.error);
       } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
+        console.log("User tapped custom button: ", response.customButton);
         alert(response.customButton);
       } else {
         const source = { uri: response.uri };
-        console.log('response', JSON.stringify(response));
+        console.log("response", JSON.stringify(response));
         this.setState({
           filePath: response,
           fileData: response.data,
-          fileUri: response.uri
+          fileUri: response.uri,
         });
       }
     });
-
-  }
+  };
 
   launchImageLibrary = () => {
     let options = {
       storageOptions: {
         skipBackup: true,
-        path: 'images',
+        path: "images",
       },
     };
     ImagePicker.launchImageLibrary(options, (response) => {
-      console.log('Response = ', response);
+      console.log("Response = ", response);
 
       if (response.didCancel) {
-        console.log('User cancelled image picker');
+        console.log("User cancelled image picker");
       } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
+        console.log("ImagePicker Error: ", response.error);
       } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
+        console.log("User tapped custom button: ", response.customButton);
         alert(response.customButton);
       } else {
         const source = { uri: response.uri };
-        console.log('response', JSON.stringify(response));
+        console.log("response", JSON.stringify(response));
         this.setState({
           filePath: response,
           fileData: response.data,
-          fileUri: response.uri
+          fileUri: response.uri,
         });
       }
     });
-
-  }
+  };
 
   renderFileData() {
     if (this.state.fileData) {
-      return <Image source={{ uri: 'data:image/jpeg;base64,' + this.state.fileData }}
-        style={styles.images}
-      />
+      return (
+        <Image
+          source={{ uri: "data:image/jpeg;base64," + this.state.fileData }}
+          style={styles.images}
+        />
+      );
     } else {
-      return <Image source={require('./assets/dummy.png')}
-        style={styles.images}
-      />
+      return (
+        <Image source={require("./assets/dummy.png")} style={styles.images} />
+      );
     }
   }
 
   renderFileUri() {
     if (this.state.fileUri) {
-      return <Image
-        source={{ uri: this.state.fileUri }}
-        style={styles.images}
-      />
+      return (
+        <Image source={{ uri: this.state.fileUri }} style={styles.images} />
+      );
     } else {
-      return <Image
-        source={require('./assets/galeryImages.jpg')}
-        style={styles.images}
-      />
+      return (
+        <Image
+          source={require("./assets/galeryImages.jpg")}
+          style={styles.images}
+        />
+      );
     }
   }
   render() {
@@ -170,38 +164,52 @@ export default class App extends Component {
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
           <View style={styles.body}>
-            <Text style={{textAlign:'center',fontSize:20,paddingBottom:10}} >Pick Images from Camera & Gallery</Text>
+            <Text
+              style={{ textAlign: "center", fontSize: 20, paddingBottom: 10 }}
+            >
+              Pick Images from Camera & Gallery
+            </Text>
             <View style={styles.ImageSections}>
               <View>
                 {this.renderFileData()}
-                <Text  style={{textAlign:'center'}}>Base 64 String</Text>
+                <Text style={{ textAlign: "center" }}>Base 64 String</Text>
               </View>
               <View>
                 {this.renderFileUri()}
-                <Text style={{textAlign:'center'}}>File Uri</Text>
+                <Text style={{ textAlign: "center" }}>File Uri</Text>
               </View>
             </View>
 
             <View style={styles.btnParentSection}>
-              <TouchableOpacity onPress={this.chooseImage} style={styles.btnSection}  >
+              <TouchableOpacity
+                onPress={this.chooseImage}
+                style={styles.btnSection}
+              >
                 <Text style={styles.btnText}>Choose File</Text>
               </TouchableOpacity>
 
-              <TouchableOpacity onPress={this.launchCamera} style={styles.btnSection}  >
+              <TouchableOpacity
+                onPress={this.launchCamera}
+                style={styles.btnSection}
+              >
                 <Text style={styles.btnText}>Directly Launch Camera</Text>
               </TouchableOpacity>
 
-              <TouchableOpacity onPress={this.launchImageLibrary} style={styles.btnSection}  >
-                <Text style={styles.btnText}>Directly Launch Image Library</Text>
+              <TouchableOpacity
+                onPress={this.launchImageLibrary}
+                style={styles.btnSection}
+              >
+                <Text style={styles.btnText}>
+                  Directly Launch Image Library
+                </Text>
               </TouchableOpacity>
             </View>
-
           </View>
         </SafeAreaView>
       </Fragment>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -210,43 +218,43 @@ const styles = StyleSheet.create({
 
   body: {
     backgroundColor: Colors.white,
-    justifyContent: 'center',
-    borderColor: 'black',
+    justifyContent: "center",
+    borderColor: "black",
     borderWidth: 1,
-    height: Dimensions.get('screen').height - 20,
-    width: Dimensions.get('screen').width
+    height: Dimensions.get("screen").height - 20,
+    width: Dimensions.get("screen").width,
   },
   ImageSections: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     paddingHorizontal: 8,
     paddingVertical: 8,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   images: {
     width: 150,
     height: 150,
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
-    marginHorizontal: 3
+    marginHorizontal: 3,
   },
   btnParentSection: {
-    alignItems: 'center',
-    marginTop:10
+    alignItems: "center",
+    marginTop: 10,
   },
   btnSection: {
     width: 225,
     height: 50,
-    backgroundColor: '#DCDCDC',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#DCDCDC",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 3,
-    marginBottom:10
+    marginBottom: 10,
   },
   btnText: {
-    textAlign: 'center',
-    color: 'gray',
+    textAlign: "center",
+    color: "gray",
     fontSize: 14,
-    fontWeight:'bold'
-  }
+    fontWeight: "bold",
+  },
 });
